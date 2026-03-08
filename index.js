6b4211a0ad8286a1b951a21f9a5d746c3b8472d3
Hjalmar Meza <hjalmar.meza@gmail.com>
2026-03-08 02:48:38 +0100

    fix: backtrack to stable webVersion

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     webVersionCache: {
         type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014581177-alpha.html',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     },
     puppeteer: {
         headless: true,
